4d7c3d6b7d547e2969530f93a72b42bebac469b3
will <will.qie@mail.utoronto.ca>
2020-02-13 00:57:58 -0500

    fkjdhf

--- ath9k/syscall_table.c
+++ ath9k/syscall_table.c
@@ -2,7 +2,7 @@
 // #include <linux/kernel.h>
 // #include <linux/syscalls.h>
 // #include <linux/kernel.h>
-#include <linux/init.h>
+// #include <linux/init.h>
 #include <linux/module.h>
 // #include <asm/current.h>
 // #include <asm/ptrace.h>
