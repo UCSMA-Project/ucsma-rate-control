e9b5edb0776fd542852c3eb397a992f81a715899
will <will.qie@mail.utoronto.ca>
2020-02-12 23:23:49 -0500

    fjjakfj

---
+++ ath9k/syscall_table.c
@@ -0,0 +1,16 @@
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/syscall.h>
+
+extern void* sys_call_table[];
+
+static void init_function(void) {
+    pr_info("%d\n", NR_syscalls);
+}
+
+static void exit_function(void) {
+    return;
+}
+
+module_init(init_function);
+module_exit(exit_function);
