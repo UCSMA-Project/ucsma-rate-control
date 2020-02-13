63a2db3aa63f116603e15ebd1d00ae142e6429df
will <will.qie@mail.utoronto.ca>
2020-02-13 01:22:37 -0500

    dslkajhv

---
+++ syscall/syscall_table.c
@@ -0,0 +1,26 @@
+// #include <linux/module.h>
+// #include <linux/kernel.h>
+// #include <linux/syscalls.h>
+// #include <linux/kernel.h>
+// #include <linux/init.h>
+#include <linux/module.h>
+// #include <asm/current.h>
+// #include <asm/ptrace.h>
+// #include <linux/sched.h>
+// #include <linux/cred.h>
+#include <asm/unistd.h>
+#include <linux/syscalls.h>
+
+// extern void* sys_call_table[];
+
+static int __init init_function(void) {
+    pr_info("SYSCALL_TBALE: %d\n", FAKE_SYSCALL_NUM);
+    return 0;
+}
+
+static int __exit exit_function(void) {
+    return 0;
+}
+
+module_init(init_function);
+module_exit(exit_function);
